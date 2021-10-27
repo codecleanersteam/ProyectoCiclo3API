0c55ba8f17d6b94ca21eb0f46d2e392d7ac592f9
Mauricio Cendales <malexcdl@hotmail.com>
2021-10-27 09:12:06 -0500

    Autenticación de token implementada

--- servidor.js
+++ servidor.js
@@ -6,12 +6,30 @@ import { dBConnect } from "./db/database.js";
 import rutasProducto from "./views/productos/rutas.js";
 import rutasUsuario from "./views/usuarios/rutas.js";
 import rutasVenta from "./views/ventas/rutas.js"
+import jwt from "express-jwt"
+import jwks from "jwks-rsa"
 
 dotenv.config({ path: "./.env" });
 
 const app = Express();
 app.use(Express.json());
 app.use(Cors());
+
+//Autenticación de Backend
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-pqc1pi-5.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "api-proyectociclo3",
+  issuer: "https://dev-pqc1pi-5.us.auth0.com/",
+  algorithms: ["RS256"],
+});
+
+app.use(jwtCheck);
+
 app.use(rutasProducto)
 app.use(rutasUsuario)
 app.use(rutasVenta)
